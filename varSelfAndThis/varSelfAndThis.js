804e14a2d35a12b85245604c46264bc181179f93
Chaynor Hsiao <chaynsaw@gmail.com>
2019-07-06 07:42:15 -0700

    Experiment with This

---
+++ varSelfAndThis/varSelfAndThis.js
@@ -0,0 +1,22 @@
+function xyz(){
+  // "this" is different here!
+  console.log(this); // not what we wanted!
+  function qwe(){
+    // "this" is different here too!
+    console.log(this); // not what we wanted!
+  }
+
+};
+
+
+// This question is not specific to jQuery, but specific to JavaScript in general. The core problem is how to "channel" a variable in embedded functions. This is the example:
+
+var abc = 1; // we want to use this variable in embedded functions
+
+function xyz(){
+  console.log(abc); // it is available here!
+  function qwe(){
+    console.log(abc); // it is available here too!
+  }
+
+};
